eda8c55e6cb10230c445805e796912d7248c6552
Guigous <gui_1810@hotmail.com>
2021-09-15 19:42:59 -0300

    Area Do Circulo

--- AreaDoCirculo/Calc.cs
+++ AreaDoCirculo/Calc.cs
@@ -10,13 +10,13 @@ namespace AreaDoCirculo
     class Calculadora
     {
         // Variaveis e metodos da Classe para os calculos
-        public double Pi = 3.14;
-        public double Circunferencia(double r)
+        public static double Pi = 3.14;
+        public static double Circunferencia(double r)
         {
             return 2.0 * Pi * r;
 
         }
-        public double Volume(double r)
+        public static double Volume(double r)
         {
             return 4.0 / 3.0 * Pi * r * r * r;
         }
--- AreaDoCirculo/Program.cs
+++ AreaDoCirculo/Program.cs
@@ -10,16 +10,16 @@ namespace AreaDoCirculo
 
         {
             // Instaciamento da classe e input do usuario
-            Calculadora calc = new Calculadora();
+            //      Calculadora calc = new Calculadora();
             Console.Write("Entre com o valor do Raio :");
             double raio = Double.Parse(Console.ReadLine());
-            double circ = calc.Circunferencia(raio);
-            double volume = calc.Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
 
             // Feedback do resultado
             Console.WriteLine("Circunferencia : " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume : " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Circunferencia : " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));  
+            Console.WriteLine("Circunferencia : " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));  
         }
     }
 }
